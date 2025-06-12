1626fee3cec4c30819551fd7b99e1c8792d3939b
CollectionofEG <148367269+Pepperhfj@users.noreply.github.com>
2025-06-12 16:55:10 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
-const yesButton = document.getElementById('yes');
+/* HELLO I DIDNT THINK YOU WOULD CHECK THE SOURCE CODE BUT ANYWAYS: 
+THIS GAME WAS MADE AFTER AN INSIDE JOKE ME AND MY BROTHER FOUND FUNNY
+WHICH WAS DEDICATED TO THIS ONE THING I SAID THAT WAS LIKE 
+"sahdow the hedgehog requests a game of five fingers up my ass" (typo included) 
+BUT I HOPE YOU LIKE IT  ^^ */
 
+const yesButton = document.getElementById('yes');
 const coolButton = document.getElementById('urcool');
 
 yesButton.onclick = cutScene2;
-
 coolButton.onclick = cutscene3;
 
 function cutScene2() {
